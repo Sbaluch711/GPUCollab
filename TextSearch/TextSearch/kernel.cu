41176ef2a2fc9eb3ad8f13bc282296ec8349f53c
Sydni Baluch <sbaluch@carthage.edu>
2017-01-25 11:54:20 -0600

    Add files via upload

--- TextSearch/TextSearch/kernel.cu
+++ TextSearch/TextSearch/kernel.cu
@@ -9,27 +9,22 @@
 using namespace std;
 
 #define GIGA (1 <<30)
-
+#define BSIZE (GIGA/8)
 int main()
 {
 	HighPrecisionTime readingTimer;
-	char* bitmap = new char[GIGA];
-	
+	char* buffer = new char[GIGA]();
+	char* bitmap = new char[BSIZE]();
+
 	ifstream bigFile("C:\\Users\\educ\\Documents\\enwiki-latest-abstract.xml");
 
 	readingTimer.TimeSinceLastCall();
-	bigFile.read(bitmap, GIGA);
+	bigFile.read(buffer, GIGA);
 	cout << readingTimer.TimeSinceLastCall() << endl;
 
 	bigFile.close();
+
 	system("pause");
 	return 0;
-	// process data in buffer
-
-	//open file
-	//time loading giga
-	//print file
-	//close
-	//exit
 }
 
